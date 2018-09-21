a3037f41c0ab412ff46d23d0af89625987283bff
taehyung.kim <taehyung.kim@seezoo.co.jp>
2018-09-21 10:30:48 +0900

    logout機能追加

--- application/controllers/Logout.php
+++ application/controllers/Logout.php
@@ -1,24 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Login extends CI_Controller {    
+class Logout extends CI_Controller {    
 
     public function __construct() {
         parent::__construct();
+        $this->session->sess_destroy();
     }
     
     public function index()
-    {   
-        
-        $this->load->model('member_dao');     
-        
-        if($this->member_dao->login($this->input->post('id'), $this->input->post('password')))
-        {
-            // login 成功
-           $this->load->view('sns_view');
-        } else {
-           // login 失敗
-           $this->load->view('main_view');
-        }                  
+    {           
+           $this->load->view('welcome_view');               
     }       
 }
