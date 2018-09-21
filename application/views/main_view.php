6d06e73c1153d74d37397bd63de576a04dae3853
taehyung.kim <taehyung.kim@seezoo.co.jp>
2018-09-21 10:32:57 +0900

    画面のディザインの変更

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -4,28 +4,32 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="A layout example that shows off a blog page with a list of posts.">
-<title>Blog &ndash; Layout Examples &ndash; Pure</title>   
+<title>Tiny SNS</title>   
     <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/grids-responsive-min.css">
-    <link rel="stylesheet" href="../assets/css/blog.css">
+    <link href="<?= base_url()?>/assets/css/blog.css" rel="stylesheet">
 </head>
 <body>
 <div id="layout" class="pure-g">
     <div class="sidebar pure-u-1 pure-u-md-1-4">
         <div class="header">
-            <h1 class="brand-title">A Sample Blog</h1>
-            <h2 class="brand-tagline">Creating a blog layout using Pure</h2>
+            <h1 class="brand-title">TINY SNS</h1>
+            <h2 class="brand-tagline">Shere Your Interests</h2>
 
             <nav class="nav">
                 <ul class="nav-list">
                     <li class="nav-item">
-                        <a class="pure-button" href="http://purecss.io">Pure</a>
+                        <a class="pure-button" href="writepost">write post</a>
                     </li>
                     <li class="nav-item">
-                        <a class="pure-button" href="http://yuilibrary.com">YUI Library</a>
+                        <a class="pure-button" href="mypage">taehyung's Page</a>
+                    </li>                    
+                    <li class="nav-item">
+                        <br>
+                        <a class="pure-button" href="logout">logout</a>
                     </li>
-                </ul>
-            </nav>
+                </ul>               
+            </nav>            
         </div>
     </div>
 
@@ -33,24 +37,30 @@
         <div>
             <!-- A wrapper for all the blog posts -->
             <div class="posts">
-                <h1 class="content-subhead">Pinned Post</h1>
+                <h1 class="content-subhead">popular Post</h1>
 
                 <!-- A single blog post -->
                 <section class="post">
                     <header class="post-header">
-                        <img width="48" height="48" alt="Tilo Mitra&#x27;s avatar" class="post-avatar" src="../assets/img/tilo-avatar.png">
+                        <img width="48" height="48" alt="Tilo Mitra&#x27;s avatar" class="post-avatar" src="<?= base_url()?>/assets/img/mikey.jpg">
 
-                        <h2 class="post-title">Introducing Pure</h2>
+                        <h2 class="post-title">LIKE数が多い人のPOST表示</h2>
 
                         <p class="post-meta">
-                            By <a href="#" class="post-author">Tilo Mitra</a> under <a class="post-category post-category-design" href="#">CSS</a> <a class="post-category post-category-pure" href="#">Pure</a>
+                            By <a href="#" class="post-author">名前(ID)</a>   <a class="post-category post-category-js" href="#">LIKE数</a>
                         </p>
                     </header>
 
                     <div class="post-description">
-                        <p>
-                            Yesterday at CSSConf, we launched Pure – a new CSS library. Phew! Here are the <a href="https://speakerdeck.com/tilomitra/pure-bliss">slides from the presentation</a>. Although it looks pretty minimalist, we’ve been working on Pure for several months. After many iterations, we have released Pure as a set of small, responsive, CSS modules that you can use in every web project.
-                        </p>
+                        <div class="post-images pure-g">
+                            <div class="pure-u-1 pure-u-md-2-3">                              
+                                    <img class="pure-img-responsive" src="<?= base_url()?>/assets/img/miso.jpg">
+                                <div class="post-image-meta">
+                                    <h3>味噌煮込みうどん</h3>
+                                </div>
+                                <p>味噌煮込みうどんは美味しいかな？ </p>
+                            </div>
+                        </div>
                     </div>
                 </section>
             </div>
@@ -60,87 +70,35 @@
 
                 <section class="post">
                     <header class="post-header">
-                        <img width="48" height="48" alt="Eric Ferraiuolo&#x27;s avatar" class="post-avatar" src="../assets/img/ericf-avatar.png">
+                        <img width="48" height="48" alt="Eric Ferraiuolo&#x27;s avatar" class="post-avatar" src="<?= base_url()?>/assets/img/minnie.png">
 
-                        <h2 class="post-title">Everything You Need to Know About Grunt</h2>
-
-                        <p class="post-meta">
-                            By <a class="post-author" href="#">Eric Ferraiuolo</a> under <a class="post-category post-category-js" href="#">JavaScript</a>
-                        </p>
-                    </header>
-
-                    <div class="post-description">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        </p>
-                    </div>
-                </section>
-
-                <section class="post">
-                    <header class="post-header">
-                        <img width="48" height="48" alt="Reid Burke&#x27;s avatar" class="post-avatar" src="../assets/img/reid-avatar.png">
-
-                        <h2 class="post-title">Photos from CSSConf and JSConf</h2>
+                        <h2 class="post-title">選択したCATEGORYのPOST（時間順）</h2>
 
                         <p class="post-meta">
-                            By <a class="post-author" href="#">Reid Burke</a> under <a class="post-category" href="#">Uncategorized</a>
+                            By <a class="post-author" href="#">名前(ID)</a>   <a class="post-category post-category-js" href="#">LIKE数</a>
                         </p>
                     </header>
 
                     <div class="post-description">
                         <div class="post-images pure-g">
-                            <div class="pure-u-1 pure-u-md-1-2">
-                                <a href="http://www.flickr.com/photos/uberlife/8915936174/">
-                                    <img alt="Photo of someone working poolside at a resort"
-                                         class="pure-img-responsive"
-                                         src="http://farm8.staticflickr.com/7448/8915936174_8d54ec76c6.jpg">
-                                </a>
-
+                            <div class="pure-u-1 pure-u-md-2-3">                              
+                                    <img class="pure-img-responsive" src="<?= base_url()?>/assets/img/nagoya.jpg">
                                 <div class="post-image-meta">
-                                    <h3>CSSConf Photos</h3>
+                                    <h3>名古屋CITY</h3>
                                 </div>
-                            </div>
-
-                            <div class="pure-u-1 pure-u-md-1-2">
-                                <a href="http://www.flickr.com/photos/uberlife/8907351301/">
-                                    <img alt="Photo of the sunset on the beach"
-                                         class="pure-img-responsive"
-                                         src="http://farm8.staticflickr.com/7382/8907351301_bd7460cffb.jpg">
-                                </a>
-
-                                <div class="post-image-meta">
-                                    <h3>JSConf Photos</h3>
-                                </div>
+                                <p>栄は夜景がいいな</p>
                             </div>
                         </div>
                     </div>
                 </section>
-
-                <section class="post">
-                    <header class="post-header">
-                        <img width="48" height="48" alt="Andrew Wooldridge&#x27;s avatar" class="post-avatar" src="../assets/img/andrew-avatar.png">
-
-                        <h2 class="post-title">YUI 3.10.2 Released</h2>
-
-                        <p class="post-meta">
-                            By <a class="post-author" href="#">Andrew Wooldridge</a> under <a class="post-category post-category-yui" href="#">YUI</a>
-                        </p>
-                    </header>
-
-                    <div class="post-description">
-                        <p>
-                            We are happy to announce the release of YUI 3.10.2! You can find it now on the Yahoo! CDN, download it directly, or pull it in via npm. We’ve also updated the YUI Library website with the latest documentation.
-                        </p>
-                    </div>
-                </section>
             </div>
-
+            
             <div class="footer">
                 <div class="pure-menu pure-menu-horizontal">
                     <ul>
-                        <li class="pure-menu-item"><a href="http://purecss.io/" class="pure-menu-link">About</a></li>
-                        <li class="pure-menu-item"><a href="http://twitter.com/yuilibrary/" class="pure-menu-link">Twitter</a></li>
-                        <li class="pure-menu-item"><a href="http://github.com/pure-css/pure/" class="pure-menu-link">GitHub</a></li>
+                        <li class="pure-menu-item"><a href="http://purecss.io/" class="pure-menu-link">About Neo</a></li>
+                        <li class="pure-menu-item"><a href="http://twitter.com/" class="pure-menu-link">Twitter</a></li>
+                        <li class="pure-menu-item"><a href="" class="pure-menu-link">Information</a></li>
                     </ul>
                 </div>
             </div>
--- application/views/welcome_view.php
+++ application/views/welcome_view.php
@@ -1,21 +1,44 @@
 <!DOCTYPE html>
 <html lang="ja">
     <head>
-        <meta charset="UTF-8">  
-        <link rel="stylesheet" href="assets/css/blog.css">
-        <title>welcome to tinySNS</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="A layout example that shows off a blog page with a list of posts.">
+    <title>Welcome Tiny SNS!</title>   
+        <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/grids-responsive-min.css">
+        <link href="<?= base_url()?>/assets/css/blog.css" rel="stylesheet">
     </head>
     <body>
-        <h1>tiny SNS</h1>
-        <form method="post" action="login">
-            <label for="id">Username</label>
-            <input type="text" id="id" name="id" placeholder="Username">
-
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" placeholder="Password">
-
-            <input type="submit" value="login">
-        </form> 
-            <a href="/tinysns/join"><input type="button" value="join"></a>
+    <div id="layout" class="pure-g">
+        <div class="sidebar pure-u-1 pure-u-md-1-4">
+            <div class="header">
+                <h1 class="brand-title">TINY SNS</h1>
+                <h2 class="brand-tagline">Shere Your Interests</h2>
+                <form method="post" action="login">
+                <nav class="nav">
+                    <ul class="nav-list">
+                        <li class="nav-item">
+                            <label>ID
+                            <input type="text" id="id" name="id">
+                            </label>
+                        </li>
+                        <li class="nav-item">
+                            <label>PASSWORD</label>
+                            <input type="password" id="password" name="password">
+                        </li>  
+                        <li>
+                            <br>
+                            <a href="/tinysns/join"><input type="button" value="join"></a>
+                            <input type="submit" value="login">
+                        </li>
+                    </ul>               
+                </nav>
+                </form>
+            </div>
+        </div>
+    </div>    
+    <div class="content pure-u-1 pure-u-md-3-4">                     
+           
     </body>
 </html>
