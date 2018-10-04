c564b1356a3fab7920af227b72cdd15d5c670737
taehyung.kim <taehyung.kim@seezoo.co.jp>
2018-10-04 16:07:35 +0900

    sqlのバーグ修正

--- application/controllers/WritePost.php
+++ application/controllers/WritePost.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class WritePost extends CI_Controller {
-    
+
     public function __construct() 
     {
         parent::__construct();
@@ -17,19 +17,35 @@ class WritePost extends CI_Controller {
     }   
     
     public function write()
-    {       
+    {   
+        $p_category = NULL;
+        
         $this->form_validation->set_rules('title', 'title', 'required');
         $this->form_validation->set_rules('p_text', 'p_text', 'required');
-        $this->form_validation->set_rules('p_category', 'p_category', 'required');
+        $this->form_validation->set_rules('p_category[]', 'p_category', 'required');
         
-        if($this->form_validation->run() == false){
-            $this->load->view('writepost_view'); 
-        } else {       
-            $this->post_dao->write($this->session->userdata('id'), $this->input->post('title'), 
-                                   $this->input->post('p_photo'), $this->input->post('p_text'), 
-                                   $this->input->post('p_category'), date('Y-m-d H:i:s'));
+        if ($this->form_validation->run() == false)
+        {
+            $this->load->view('writepost_view');            
+        } else {  
             
-            $this->load->view('main_view');           
+            $category_array = $this->input->post('p_category');
+            
+            foreach ($category_array as $value) 
+            {           
+                $p_category .= $value;
+            }
+            
+            $post_array = [ $this->session->userdata('id'), $this->input->post('title'), 
+                            $this->input->post('p_photo'), $this->input->post('p_text'), 
+                            $p_category, date('Y-m-d H:i:s') ];
+            
+            $post_result = $this->post_dao->write($post_array);
+            
+            if ($post_result != FALSE)
+            {
+                redirect('main');       
+            }          
         }       
     }
 }
